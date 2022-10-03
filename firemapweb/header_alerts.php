ddbd0ba928456e6e73bfdfd48dbd1a2f38346dc4
Dave Mateer <davemateer@gmail.com>
2022-10-03 09:22:16 +0100

    Fix <? to <?php to stop parser error

--- firemapweb/header_alerts.php
+++ firemapweb/header_alerts.php
@@ -15,7 +15,7 @@ if ($webserver != "azureprod") {
 	<link rel="stylesheet" href="style.css" />
 	<p style="font-size: 75%"> Menu: [Logged in as: <?php echo $userid; ?>]&nbsp&nbsp&nbsp&nbsp<a href="projectlist_you.php">[Your Projects]</a>&nbsp&nbsp&nbsp&nbsp<a href="addproject.php">[Add Project]</a>&nbsp&nbsp&nbsp&nbsp<a href="changepassword.php">[Change Password]</a>&nbsp&nbsp&nbsp&nbsp<a href="logout.php">[Log out]</a>
 	</p>
-<?
+<?php
 // development - stop script here
 die();
 }
