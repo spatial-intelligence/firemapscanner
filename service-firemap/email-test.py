96a546765a4c0a04d4e27efadf81974442ff7cd5
Dave Mateer <davemateer@gmail.com>
2022-10-11 10:28:04 +0100

    Adding Pipfile for dependencies in development

---
+++ service-firemap/email-test.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+# https://postmarkapp.com/send-email/python
+from postmarker.core import PostmarkClient
+import postmarker
+
+# 3.9.14
+print("Python intepreter version:")
+print(sys.version)
+
+# 1.0
+print("Postmarker version:")
+print(postmarker.__version__)
+
+# token = os.environ['POSTMARK_TOKEN']
+token = os.environ.get('POSTMARK_TOKEN')
+
+# send to black hole (will succeed if no errors)
+if token is None:
+  token = 'POSTMARK_API_TEST'
+
+postmark = PostmarkClient(server_token=token)
+postmark.emails.send(
+  From='help@osr4rightstools.org',
+  To='davemateer@gmail.com',
+  Subject='Postmark test',
+  HtmlBody='Successful test - this is html'
+)
